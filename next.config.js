3ce6310a114ccc9a2a1ce5b6db32acd15ea460d3
Huriel Becker <becker.programador.com>
2023-05-13 14:35:47 -0300

    Modificado para SSR funcionar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+/* eslint-disable no-undef */
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true };
+
+module.exports = nextConfig;
