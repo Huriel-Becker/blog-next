1f54deb72ad2b654693deab96c90ce8801dafef4
Huriel Becker <becker.programador.com>
2023-05-13 15:36:08 -0300

    Criação out_publish

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
-/* eslint-disable no-undef */ /** @type {import('next').NextConfig} */ const nextConfig =
-  { reactStrictMode: true };
+/* eslint-disable no-undef */
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true, distDir: 'out_publish' };
 module.exports = nextConfig;
