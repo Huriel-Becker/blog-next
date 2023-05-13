271c4b6c551363075c589528a1b6e3b26f07b96a
Huriel Becker <becker.programador.com>
2023-05-12 21:41:03 -0300

    Modificado [category] para  SSR

--- src/pages/categories/[category].tsx
+++ src/pages/categories/[category].tsx
@@ -1,7 +1,12 @@
-import { GetServerSideProps } from 'next';
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from 'next';
 import HomePage from '../../containers/HomePage';
 import { getAllPosts } from '../../data/posts/get-all-posts';
 import { PostData } from '../../domain/posts/post';
+import { ParsedUrlQuery } from 'querystring';
 
 export type CategoryProps = {
   posts: PostData[];
@@ -12,11 +17,14 @@ export default function Category({ posts, category }: CategoryProps) {
   return <HomePage category={category} posts={posts} />;
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const urlQuery = `sort=id:desc&pagination[start]=0&pagination[limit]=10&sort=id:DESC&filters[$and][0][category][name][$eq]=${ctx.query.category}`;
+export const getServerSideProps: GetServerSideProps<CategoryProps> = async (
+  ctx: GetServerSidePropsContext<ParsedUrlQuery>,
+) => {
+  const category = ctx.query.category as string;
+  const urlQuery = `sort=id:desc&pagination[start]=0&pagination[limit]=10&sort=id:DESC&filters[$and][0][category][name][$eq]=${category}`;
   const posts = await getAllPosts(urlQuery);
 
   return {
-    props: { posts, category: ctx.query.category },
-  };
+    props: { posts, category },
+  } as GetServerSidePropsResult<CategoryProps>;
 };
